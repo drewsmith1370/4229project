9515b2123628ecbcbd8635426b10cc32a6684f61
drew <drsm1370@colorado.edu>
2024-11-24 15:53:02 -0700

    init boilderplate

---
+++ sample/main.c
@@ -0,0 +1,10 @@
+#include "CSCIx229.h"
+#include <GLFW/glfw3.h>
+
+int main(int argc, char** argv) {
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API,GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE,GLFW_TRUE);
+
+    return 0;
+}
